c80d958c78bb4456d36eaa6ed25e8af29dd7cd23
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-04-01 11:43:28 +0530

    added prod value

--- frontend/src/data/config.js
+++ frontend/src/data/config.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:4000/api";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://dashboard-rocklime.onrender.com/api"
+    : "http://localhost:4000/api";
