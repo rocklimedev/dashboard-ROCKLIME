9a009a43afb53acf65d6563cb40d72fec3e93ae4
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-05-17 11:32:43 +0530

    added much of i9t

--- frontend/src/data/config.js
+++ frontend/src/data/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://dashboard-rocklime-production.up.railway.app/api"
+    ? "https://dashboard-rocklime.onrender.com/api"
     : "http://localhost:4000/api";
