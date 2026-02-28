4affd91b5c2ab35119ab34fbd29608a63b39b344
Dhruvermafz <vermadhruv09112002@gmail.com>
2026-02-28 12:34:25 +0530

    cleaned the whole

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -17,7 +17,5 @@ root.render(
         </AuthProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-reportWebVitals();
